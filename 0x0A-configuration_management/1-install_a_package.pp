a70557a4d2409c09a1a1ff717e514130dd64af7d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-28 16:10:47 +0100

    Update Flask installation method

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #INSTALL flask from pip3
-exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
