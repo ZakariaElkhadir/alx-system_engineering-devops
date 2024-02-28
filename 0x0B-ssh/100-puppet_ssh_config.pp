48f391df9ef86e4f3c697a45f874bbcdedb9eeb3
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-28 18:45:12 +0100

    Add SSH configuration to disable password authentication and declare identity file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,14 @@
 #client configuration using puppet
 
-file { '~/.ssh/school':
+file { 'turn off password authentication':
   ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
+
+file { 'declare identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school',
+}
+
