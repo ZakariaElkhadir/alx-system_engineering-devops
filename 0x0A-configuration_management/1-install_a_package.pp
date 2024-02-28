c4547269066be1a05958ed5702a7e41def64a837
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-28 15:56:12 +0100

    add file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #INSTALL flask from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
