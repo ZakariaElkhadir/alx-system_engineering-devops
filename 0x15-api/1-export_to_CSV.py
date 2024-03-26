2073f41a3d47bec618a106815ae4039212f536cb
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-26 02:14:03 +0000

    add file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-script that, using this REST API,
-for a given employee ID,
-returns information
-"""
+"""script to export data in the CSV format"""
+import csv
 import requests
 import sys
 
 
 def main():
-    """ api request"""
+    """api_request"""
     url_task = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
         int(sys.argv[1]))
     url_name = "https://jsonplaceholder.typicode.com/users/{}".format(
@@ -20,13 +17,17 @@ def main():
     data_name = response_name.json().get('name')
     task_done = ['\t {}\n'.format(dic.get('title')) for dic in data
                  if dic.get('completed') is True]
-
     if data_name and data:
         print("Employee {} is done with tasks({}/{}):".format(
             data_name, len(task_done), len(data)))
         print("".join(task_done), end='')
+        with open('{}.csv'.format(sys.argv[1]), mode='w', newline='') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            for dic in data:
+                writer.writerow([dic.get('userId'), data_name, dic.get(
+                    'completed'), dic.get('title')])
 
 
 if __name__ == "__main__":
-    """ Entry point"""
+    """Entry point"""
     main()
