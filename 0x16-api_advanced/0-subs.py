b902050106ee39a4b011524182b6eb33826f2693
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-04-10 18:26:08 +0000

    edit file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,18 +6,16 @@ headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
 
 
 def number_of_subscribers(subreddit):
-  """method doc"""
-  url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-  try:
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    data = response.json()
-    return data.get('data', {}).get('subscribers', 0)
-  except requests.exceptions.RequestException as e:
-    print(f"Request failed: {e}")
-    return 0
-  except (ValueError, TypeError) as e:
-    print(f"JSON decoding error: {e}")
-    return 0
-  if not subreddit:
-    return 0
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    try:
+      response = requests.get(url, headers=headers)
+      response.raise_for_status()
+      data = response.json()
+      return data.get('data', {}).get('subscribers', 0)
+    except requests.exceptions.RequestException as e:
+      print(f"Request failed: {e}")
+      return 0
+    except (ValueError, TypeError) as e:
+      print(f"JSON decoding error: {e}")
+      return 0
