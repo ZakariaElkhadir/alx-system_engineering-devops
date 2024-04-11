c44429aa5b198a039e0f2b3f9fff11fa6a77e787
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-04-11 11:53:20 +0000

    Add docstring to top_ten function in 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,6 +6,7 @@ import requests
 
 
 def top_ten(subreddit):
+    """ Get the top 10 hot posts for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     response = requests.get(url, headers=headers)
