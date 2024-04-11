d59bd57310887736afbb446136fe902dfbb504ff
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-04-11 01:17:37 +0000

    edit file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,21 @@
 import requests
 import json
 
+
 def number_of_subscribers(subreddit):
-    """doc doc"""
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+
+    """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = json.loads(response.text)
