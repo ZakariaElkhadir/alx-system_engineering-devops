074bce73becfa80b77ed1f29a50a57cdb7662466
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-06-19 09:59:57 +0100

    edit file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,19 @@
 #!/use/bin/python3
+""" 0-subs.py """
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+          Returns 0 if an error occurs.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "subreddit-subscriber-count-script/1.0"}
 
