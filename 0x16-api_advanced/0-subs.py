c0373ce41c23d66a982d1ac05548278952d02248
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-06-19 10:13:02 +0100

    edit file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,17 @@
-#!/use/bin/python3
-""" 0-subs.py """
-import requests
+#!/usr/bin/python3
+"""0-subs module"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    Get the number of subscribers for a given subreddit.
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for the subreddit.
-          Returns 0 if an error occurs.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "subreddit-subscriber-count-script/1.0"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data["data"]["subscribers"]
-        else:
-            return 0
-    except Exception:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-if __name__ == "__main__":
-    number_of_subscribers()
+    return sub_info.json().get("data").get("subscribers")
